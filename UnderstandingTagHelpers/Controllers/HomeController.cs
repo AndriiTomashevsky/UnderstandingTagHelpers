2110307bbe00f2fd66152116926791e75eeb8e1f
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-05-09 11:40:29 +0300

    Add project files.

---
+++ UnderstandingTagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnderstandingTagHelpers.Models;
+
+namespace UnderstandingTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Cities);
+
+        [HttpGet]
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(City city)
+        {
+            repository.AddCity(city);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
